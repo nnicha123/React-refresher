58cdd81814039c01d2a2ec0f49214ff2d9c34a3b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-12-11 10:49:22 +0700

    Finished warmup app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // 1) Import React and ReactDOM libraries
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
 // 2) Get a reference to the div with ID root
